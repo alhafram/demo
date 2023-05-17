16e8fa4e24dc0852909431855d9360bd49a723b7
Albert <alhafram@gmail.com>
2023-05-17 10:21:59 +0300

    WIP swipe

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:demo/content_data_source.dart';
-import 'package:illustration/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:illustration/home_screen_provider.dart';
+import 'package:illustration/router.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-      create: (_) =>
-          HomeScreenProvider(ContentDataSource(delegate: Delegate())),
-      child: const MyApp()));
+  runApp(
+    ChangeNotifierProvider(
+        create: (_) =>
+            HomeScreenProvider(ContentDataSource(delegate: Delegate())),
+        child: const MyApp()),
+  );
+  appRouter.go('/home');
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Flutter Demo', home: HomeScreen());
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      routeInformationProvider: appRouter.routeInformationProvider,
+      routeInformationParser: appRouter.routeInformationParser,
+      routerDelegate: appRouter.routerDelegate,
+    );
   }
 }
